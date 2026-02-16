6eb299facd42e7a5d9793bf517ecc4c6c3db1836
Rohit Khandelwal <rohitkhandelwal@Rohits-MacBook-Air.local>
2026-02-16 09:25:51 +0530

    Chapter-9 7 Criminal Intent start

---
+++ CriminalIntentImplementation/app/src/main/java/com/bignerdranch/criminalintent/Crime.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.criminalintent
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime(
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean,
+    val onCheckBoxClick: (Boolean) -> Unit
+)
