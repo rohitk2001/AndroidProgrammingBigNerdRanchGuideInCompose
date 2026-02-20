8b3ab06e98a8237aa3b13820bfe8b82cb9450e71
Rohit Khandelwal <rohitkhandelwal@Rohits-MacBook-Air.local>
2026-02-20 14:33:40 +0530

    Chapter 11

--- CriminalIntentImplementation/app/src/main/java/com/bignerdranch/criminalintent/CrimeListFragment.kt
+++ CriminalIntentImplementation/app/src/main/java/com/bignerdranch/criminalintent/CrimeListFragment.kt
@@ -6,14 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -22,14 +26,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import java.util.Date
 import java.util.UUID
+import android.text.format.DateFormat
 
 class CrimeListFragment : Fragment() {
     private val crimeListViewModel: CrimeListViewModel by viewModels()
@@ -68,12 +78,34 @@ private fun CrimeList(crimes: List<Crime>) {
 fun CrimeListItem(crime: Crime) {
     var showDialog by remember { mutableStateOf(false) }
 
-    Column(
+    Row(
         modifier = Modifier
-            .padding(8.dp)
-            .clickable { showDialog = true }) {
-        Text(text = crime.title)
-        Text(text = crime.date.toString())
+            .clickable { showDialog = true }
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Text(
+                text = crime.title,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium,
+                fontFamily = FontFamily.SansSerif
+            )
+            Text(
+                text = DateFormat.format("EEEE, MMM dd, yyyy", crime.date).toString()
+            )
+        }
+
+        if (crime.isSolved) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                tint = Color.Gray,
+                contentDescription = "Arrow_Icon"
+            )
+        }
     }
 
     if (showDialog) {
